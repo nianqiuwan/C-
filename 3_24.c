2b9b5cf1bd49ff1cb8c4a0f0d2ddacadefb7ae02
念秋晚 <2896780148@qq.com>
2026-03-25 13:41:43 +0800

    Add files via upload

---
+++ 3_24.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//// 枚举常量
+//enum color
+//{
+//	RED,	// 注意后面是逗号
+//	GREEN,
+//	BLUE,
+//};
+//// define 定义的标识符常量
+//#define MAX 100		// 注意后面没有分号
+//#define STR "abcd"
+//int main()
+//{
+//	printf("%d\n", MAX);
+//	int a = MAX;
+//	printf("%d\n", a);
+//	printf("%s\n", STR);
+//
+//
+//	return 0;
+//}
+
+
+//#include  <string.h>	// 字符串函数头文件
+//int main()
+//{
+//	char arr[] = "abcd";
+//	int len = strlen(arr);	// 求字符串长度的函数,string length
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+
+// 转义字符
+// \? 防止被拆解析成三字母词
+// %d 打印整型
+// %c 打印字符
+// %s 打印字符串
+// %f 打印float类型的数据
+// %lf 打印double类型的数据
+// %zu 打印sizeof的返回值
+// \\ 打印\
+
+//int main()
+//{
+//	// 定义一个选择变量
+//	int input = 0;
+//	// 询问是否好好学习
+//	printf("要好好学习吗（1/0）?");
+//	// 输入选择
+//	scanf("%d", &input);
+//	// 判断选择
+//	if (input == 1)
+//	{
+//		// 输入1的结果
+//		printf("拿offer\n");	// 结尾必须带\n不然会报错
+//	}
+//	else
+//	{
+//		// 输入0的结果
+//		printf("卖红薯\n");
+//	}
+//	return 0;
+//}
+
+
+
