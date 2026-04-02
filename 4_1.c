9b2569170c26d615753ffc1a20ffcf54dcbd9f7c
念秋晚 <2896780148@qq.com>
2026-04-02 12:29:39 +0800

    Add files via upload

---
+++ 4_1.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//// ц╟ещеепР
+//void bubble_sort(int arr[],int sz)
+//{
+//	// лкйЩ
+//	int i = 0;
+//	for (i = 0;i < sz - 1;i++)
+//	{
+//		for (int j = 0;j < sz - 1 - i;j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[] = {9,2,6,8,7,3,1};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	// ц╟ещеепР╣дкЦ╥╗
+//	bubble_sort(arr,sz);
+//	for (int i = 0;i < sz;i++)
+//	{
+//		printf("%d ",arr[i]);
+//	}
+//
+//
+//	return 0;
+//}
+
+
