6fbe956c400e8e8fd6f5fbec88154f35940315f9
念秋晚 <2896780148@qq.com>
2026-04-07 12:23:29 +0800

    Add files via upload

---
+++ 4_6.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+// & 二进制位与
+// | 二进制位或
+// ^ 二进制异或
+// 不创建临时变量来交换两个整数
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	printf("a=%d b=%d\n", a, b);
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a=%d b=%d\n", a, b);
+//}
