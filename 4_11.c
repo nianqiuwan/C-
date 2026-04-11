68319354ef07ef031ee79ac0845b24c856887648
念秋晚 <2896780148@qq.com>
2026-04-11 16:00:05 +0800

    Add files via upload

---
+++ 4_11.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//#include <assert.h>
+// 拷贝字符
+//char* my_strcpy(char* dest,const char* src)
+//{
+//	char* ret = dest;
+//	// 断言
+//	assert(src != NULL);
+//	assert(dest != NULL);
+//	while (*dest++ = *src++)// \0本质是0
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr[20] = { 0 };
+//	char arr1[] = "hello world";
+//	printf("%s", my_strcpy(arr, arr1));
+//	return 0;
+//}
+
+// 求字符串长度
+//#include <assert.h>
+//int my_strlen(const char* str)
+//{
+//	int count = 0;
+//	assert(str);
+//	while (*str++)
+//	{
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "hello world";
+//	printf("%d\n", my_strlen(arr));
+//	return 0;
+//}
+
+
